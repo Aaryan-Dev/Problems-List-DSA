346d3ccc8ea58fdf1a7d32e540c1acb53aa688d5
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-04-27 09:36:41 +0530

    Update Count such pairs.js

--- Count such pairs.js
+++ Count such pairs.js
@@ -2,21 +2,20 @@
 
 // Your task is to find out the number of pairs of integers present in the array, whose sum is equal to the target 
 
-
 function countSuchPairs(N,K,A){
    
-    let count = 0;
-for(i=0; i<A.length; i++){
-    let sum = 0;
-for(j=i; j<A.length; j++){
-    sum = sum + A[j];
- //   console.log(sum);
-    if(sum===K){
+    count= 0;
+for(i=0; i<N; i++){
+    
+    // var sum = 0;
+for(j=i+1; j<N; j++){
+    
+    sum = A[i] + A[j]; 
+    // console.log(sum);
+    if(sum==K){
         count++
     }
 }
 }
-console.log(count);
-
+    console.log(count);
 }
-valueK
